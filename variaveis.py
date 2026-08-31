c8e857e46ae869cadb94983a64dbc35edf67497c
ezequiel <ezequielsmup@gmail.com>
2026-08-31 19:30:46 -0400

    bug nr 23 corrigido

---
+++ variaveis.py
@@ -0,0 +1,10 @@
+nota_1 = int(input('digite a primeira nota: '))
+nota_2 = int(input('digite a segunda nota: '))
+media = nota_1 + nota_2 / 2
+
+
+if media <= 7:
+    print('VOCE ESTA REPROVado')
+else:
+    print('voce esta aprovado')
+    
